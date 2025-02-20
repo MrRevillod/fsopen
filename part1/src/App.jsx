f0ceaec77e3d0f6f30ce31dde69fe3759a7b63dd
MrRevillod <lrevillod2022@alu.uct.cl>
2025-02-20 18:29:57 -0300

    feat: :fire: exercises ...1.14

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,56 +1,51 @@
 import { useState } from "react";
 
-const StatisticLine = (props) => {
-  return (
-    <p>
-      {props.text} {props.value}
-    </p>
-  );
-};
-
-const Statistics = (props) => {
-  const total = props.good + props.neutral + props.bad;
-  const avg = (props.good - props.bad) / total;
-  const positive = (props.good / total) * 100;
-
-  return (
-    <div>
-      <h1>statistics</h1>
-
-      {total === 0 ? (
-        <p>No feedback given</p>
-      ) : (
-        <>
-          <StatisticLine text="good" value={props.good} />
-          <StatisticLine text="neutral" value={props.neutral} />
-          <StatisticLine text="bad" value={props.bad} />
-          <StatisticLine text="all" value={total} />
-          <StatisticLine text="average" value={avg} />
-          <StatisticLine text="positive" value={positive + "%"} />
-        </>
-      )}
-    </div>
-  );
-};
-
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  const [votes, setVotes] = useState(
+    Array.from({ length: anecdotes.length }, () => 0)
+  );
+
+  const onVote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
+
+  const genRandom = () => Math.floor(Math.random() * anecdotes.length);
+  const indexOfMax = votes.indexOf(Math.max(...votes));
 
   return (
     <div>
       <div>
-        <h1>give feedback</h1>
+        <h1>Anecdote of the day</h1>
+        {anecdotes[selected]}
+        <p>has {votes[selected]} votes</p>
+      </div>
+      <div>
+        <button onClick={() => onVote()}>vote</button>
+        <button onClick={() => setSelected(genRandom())}>Next anecdote</button>
+      </div>
+      <div>
+        <h2>Anecdote with most votes</h2>
         <div>
-          <button onClick={() => setGood((prev) => prev + 1)}>good</button>
-          <button onClick={() => setNeutral((prev) => prev + 1)}>
-            neutral
-          </button>
-          <button onClick={() => setBad((prev) => prev + 1)}>bad</button>
+          {anecdotes[indexOfMax]}
+          <br />
+          has {votes[indexOfMax]} votes
         </div>
-        <Statistics good={good} neutral={neutral} bad={bad} />
+        <div></div>
       </div>
     </div>
   );
